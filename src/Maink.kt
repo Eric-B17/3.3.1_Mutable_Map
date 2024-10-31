04cc7acdab399bf848055b34a4e62d4c435eb3dd
Bishoeri000 <Bishoeri000@warren.k12.in.us>
2024-10-31 12:26:38 -0400

    First

---
+++ src/Maink.kt
@@ -0,0 +1,14 @@
+fun main () {
+    val stateCapitals = mutableMapOf(
+        "Indiana" to "Indianapolis",
+        "Ohio" to "Columbus",
+        "Illinois" to "Springfield"
+    );
+
+    stateCapitals["Kentucky"] = "Frankfurt";
+    stateCapitals["Illinois"] = "Chicago";
+    stateCapitals.remove("Ohio");
+    println(stateCapitals["Kentucky"])
+    println(stateCapitals);
+
+}
